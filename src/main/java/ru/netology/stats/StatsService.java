8a28e5d33d27bf634ead1a4c4e71958800e0890f
MiScarn <bringmenoize@gmail.com>
2023-03-09 21:01:53 -0500

    6 tests

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,55 @@
+package ru.netology.stats;
+
+import org.jetbrains.annotations.NotNull;
+
+public class StatsService {
+    public long sumOfAllSales(long[] sales) {
+        long totalSale = 0;
+        for (int i = 0; i < sales.length; i++) {
+            totalSale = totalSale + sales[i];
+        }
+        return totalSale;
+    }
+    public long mean(long[] sales){
+        return sumOfAllSales(sales) / sales.length;
+    }
+
+    public int maxMonth(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+        }
+    public int minMonth(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+    public int monthlySalesBelowMean(long[] sales){
+        int result = 0;
+        for (long sale : sales){
+            if (sale < mean(sales))
+                    result ++;
+        }
+        return result;
+    }
+    public int monthlySalesAboveMean(long[] sales){
+        int result = 0;
+        for (long sale : sales){
+            if (sale > mean(sales))
+                result ++;
+        }
+        return result;
+    }
+    }
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,76 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+    public void testSumOfAllSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 180;
+        long actual = service.sumOfAllSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMean() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 180 / 12;
+        long actual = service.mean(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMaxMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 8;
+        long actual = service.maxMonth(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMinMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 9;
+        long actual = service.minMonth(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMonthlySalesBelowMean() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 5;
+        long actual = service.monthlySalesBelowMean(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testMonthlySalesAboveMean() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 5;
+        long actual = service.monthlySalesAboveMean(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
